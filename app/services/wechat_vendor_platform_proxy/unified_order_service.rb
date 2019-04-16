020d0b8fbf67af82fe697ccf246d19be8909218f
Andersen Fan <as181920@gmail.com>
2019-04-16 18:59:37 +0800

    Add UnifiedOrderService

---
+++ app/services/wechat_vendor_platform_proxy/unified_order_service.rb
@@ -0,0 +1,56 @@
+module WechatVendorPlatformProxy
+  class UnifiedOrderService
+    attr_reader :vendor
+
+    class << self
+      def perform(order_params={})
+        new(get_vendor(order_params[:mch_id])).perform(order_params)
+      end
+
+      private
+        def get_vendor(mch_id)
+          ::WechatVendorPlatformProxy::Vendor.find_by!(mch_id: mch_id)
+        end
+    end
+
+    def initialize(vendor)
+      @vendor = vendor
+    end
+
+    # order_params example:
+    #   {
+    #     appid: "",
+    #     mch_id: "",
+    #     partner_trade_no: "",
+    #     body: "",
+    #     notify_url: "",
+    #     trade_type: "",
+    #     total_fee: 100,
+    #     product_id: ""
+    #   }
+    def perform(order_params={})
+      request_params = generate_order_params(order_params)
+      call_order_api(request_params)
+    end
+
+    private
+      def sign_params(p)
+        # Digest::MD5.hexdigest(p.sort.map{|k, v| "#{k}=#{v}" }.join("&").to_s + "&key=#{key}").upcase
+        Digest::MD5.hexdigest("#{URI.unescape(p.to_query)}&key=#{vendor.sign_key}").upcase
+      end
+
+      def generate_order_params(base_params)
+        base_params.reverse_merge(
+          nonce_str: SecureRandom.hex,
+          spbill_create_ip: Socket.ip_address_list.detect{|intf| intf.ipv4_private?}.ip_address
+        ).tap { |p| p[:sign] = sign_params(p) }
+      end
+
+      def call_order_api(request_params)
+        Rails.logger.info "WechatVendorPlatformProxy UnifiedOrderService call order api reqt: #{request_params.to_json}"
+        resp = Faraday.post "https://api.mch.weixin.qq.com/pay/unifiedorder", request_params.to_xml(dasherize: false)
+        Rails.logger.info "WechatVendorPlatformProxy UnifiedOrderService call order api resp(#{resp.status}):\n#{resp.body}"
+        Hash.from_xml(resp.body)["xml"]
+      end
+  end
+end
