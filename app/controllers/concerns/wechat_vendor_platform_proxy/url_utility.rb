0886182b7f6c96ee2211cb1008cfb07041c50717
Andersen Fan <as181920@gmail.com>
2020-06-01 22:30:16 +0800

    Fix UrlUtility remove useless '?'

--- app/controllers/concerns/wechat_vendor_platform_proxy/url_utility.rb
+++ app/controllers/concerns/wechat_vendor_platform_proxy/url_utility.rb
@@ -3,7 +3,7 @@ module WechatVendorPlatformProxy
     def url_with_additional_params(url, params={})
       uri = URI(CGI.unescape(url))
       new_params = URI.decode_www_form(uri.query || '').reject{|k,v| params.stringify_keys.keys.include?(k)} + params.select{|_,v| v.present? }.to_a
-      uri.query = URI.encode_www_form(new_params)
+      uri.query = URI.encode_www_form(new_params).presence
       uri.to_s
     end
   end
