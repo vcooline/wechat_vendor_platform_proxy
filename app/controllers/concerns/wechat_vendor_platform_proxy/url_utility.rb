8a048930db0d7ff47d64eb9515bac87e16c7cbeb
Andersen Fan <as181920@gmail.com>
2020-06-17 14:04:25 +0800

    Fix url_with_additional_params duplicat unescape url params

--- app/controllers/concerns/wechat_vendor_platform_proxy/url_utility.rb
+++ app/controllers/concerns/wechat_vendor_platform_proxy/url_utility.rb
@@ -1,7 +1,7 @@
 module WechatVendorPlatformProxy
   module UrlUtility
     def url_with_additional_params(url, params={})
-      uri = URI(CGI.unescape(url))
+      uri = URI(url)
       new_params = URI.decode_www_form(uri.query || '').reject{|k,v| params.stringify_keys.keys.include?(k)} + params.select{|_,v| v.present? }.to_a
       uri.query = URI.encode_www_form(new_params).presence
       uri.to_s
