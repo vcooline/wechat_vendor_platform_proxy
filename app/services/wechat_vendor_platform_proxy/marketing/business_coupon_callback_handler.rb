7b255f811cf62e2f8cb67c00e25fadd95238f31a
Andersen Fan <as181920@gmail.com>
2022-05-01 03:00:14 +0800

    Allow coupon not found on callback handler

--- app/services/wechat_vendor_platform_proxy/marketing/business_coupon_callback_handler.rb
+++ app/services/wechat_vendor_platform_proxy/marketing/business_coupon_callback_handler.rb
@@ -13,7 +13,7 @@ module WechatVendorPlatformProxy
       private
 
         def handle_coupon_send_event(resource_params)
-          BusinessCoupon::Coupon.find_by!(code: resource_params["coupon_code"])&.then do |coupon|
+          BusinessCoupon::Coupon.find_by(code: resource_params["coupon_code"])&.then do |coupon|
             coupon.update! \
               receive_time: resource_params["send_time"],
               state: (coupon.ready? ? :sended : coupon.state)
