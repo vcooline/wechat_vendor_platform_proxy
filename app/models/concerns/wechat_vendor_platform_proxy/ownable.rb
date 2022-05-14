b3852bd6574d37f663e1cf620d8bc77479589309
Andersen Fan <as181920@gmail.com>
2022-05-14 14:36:09 +0800

    Add state fields to ecommerce applyment

--- app/models/concerns/wechat_vendor_platform_proxy/ownable.rb
+++ app/models/concerns/wechat_vendor_platform_proxy/ownable.rb
@@ -4,6 +4,7 @@ module WechatVendorPlatformProxy
 
     included do
       has_many :wxpay_sub_applyments, class_name: "::WechatVendorPlatformProxy::SubApplyment", as: :owner, dependent: :destroy
+      has_many :wxpay_ecommerce_applyments, class_name: "::WechatVendorPlatformProxy::ECommerce::Applyment", as: :owner, dependent: :destroy
     end
   end
 end
--- db/migrate/20220511094834_create_wechat_vendor_platform_proxy_e_commerce_applyments.rb
+++ db/migrate/20220511094834_create_wechat_vendor_platform_proxy_e_commerce_applyments.rb
@@ -1,6 +1,6 @@
 class CreateWechatVendorPlatformProxyECommerceApplyments < ActiveRecord::Migration[7.0]
   def change
-    create_table :wechat_vendor_platform_proxy_e_commerce_applyments do |t|
+    create_table :wxpay_ecommerce_applyments do |t|
       t.belongs_to :owner, polymorphic: true, index: { name: "index_wxpay_ecommerce_applyments_on_owner" }
       t.integer :state
       t.string :out_request_no, index: { unique: true, name: "index_wxpay_ecommerce_applyments_on_out_request_no" }
@@ -16,6 +16,15 @@ class CreateWechatVendorPlatformProxyECommerceApplyments < ActiveRecord::Migrati
       t.jsonb :qualifications
       t.jsonb :business_addition_pics
       t.text :business_addition_desc
+
+      t.string :applyment_id
+      t.text :state_desc
+      t.string :sign_state
+      t.string :sign_url
+      t.jsonb :account_validation
+      t.jsonb :audit_detail
+      t.string :legal_validation_url
+      t.string :sub_mch_id
 
       t.timestamps
     end
