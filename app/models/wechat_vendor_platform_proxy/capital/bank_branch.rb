90016b5bb476f3fc0bf19f9df39ed6b45872d750
Andersen Fan <as181920@gmail.com>
2023-10-31 12:15:40 +0800

    Fix: set ransackable on bank branch

--- app/models/wechat_vendor_platform_proxy/capital/bank_branch.rb
+++ app/models/wechat_vendor_platform_proxy/capital/bank_branch.rb
@@ -4,5 +4,13 @@ module WechatVendorPlatformProxy
     belongs_to :corporate_bank, foreign_key: :bank_alias_code, primary_key: :bank_alias_code, optional: true
 
     validates :bank_alias_code, :bank_branch_name, :bank_branch_id, :province_name, :city_name, presence: true
+
+    def self.ransackable_attributes(_auth_object = nil)
+      %w[bank_alias_code bank_branch_name bank_branch_id province_name province_code city_name city_code]
+    end
+
+    def self.ransackable_associations(_auth_object = nil)
+      %w[personal_bank corporate_bank]
+    end
   end
 end
